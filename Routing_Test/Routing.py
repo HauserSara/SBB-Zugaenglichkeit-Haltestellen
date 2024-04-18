ea0d95daa32a5ad344a373be48c960cc37905f0d
HauserSara <sara.hauser@roggli.org>
2024-04-18 11:51:13 +0200

    function height profile

--- Routing_Test/Routing.py
+++ Routing_Test/Routing.py
@@ -1,56 +1,10 @@
 import pandas as pd
+from functions import get_stop_places, get_route
+from pyproj import Transformer
+import json
 import requests
 
 data = pd.read_csv('Start_Ziel.csv')
-
-######################## Function API request stop places #########################
-def get_stop_places(X, Y, distance=500):
-    params = {
-        "where": f"within_distance(geopos_haltestelle, geom'Point({X} {Y})',{distance}m)",
-        "limit": 20
-    }
-
-    url = "https://data.sbb.ch/api/explore/v2.1/catalog/datasets/haltestelle-haltekante/records"
-    response = requests.get(url, params=params)
-    print(response.url)
-
-    if response.status_code == 200:
-        stop_places = response.json()
-        return stop_places
-    else:
-        print(f"Error: Failed to retrieve data for coordinates {X}, {Y}")
-        return None
-    
-######################## Function API request routing #########################
-def get_route(X, Y, stop_place, type):
-    params = {
-        "client": "webshop",
-        "clientVersion": "latest",
-        "lang": "de",
-        "accessible": True,
-        "api_key": "59b20b25750f56bf888ef149873f24da"
-    }
-
-    if type == "start":
-        params["fromLatLng"] = f"{Y},{X}"
-        params["toStationID"] = stop_place
-    elif type == "dest":
-        params["fromStationID"] = stop_place
-        params["toLatLng"] = f"{Y},{X}"
-    else:
-        print(f"Error: Invalid type {type}. Expected 'start' or 'dest'.")
-        return None
-
-    url = "https://journey-maps.api.sbb.ch/v1/transfer"
-    
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        route = response.json()
-        return route
-    else:
-        print(f"Error: Failed to retrieve data for coordinates {X}, {Y}")
-        return None
 
 # starting coordinates
 X1 = data['X'][0]
@@ -60,24 +14,87 @@ Y1 = data['Y'][0]
 X2 = data['X'][1]
 Y2 = data['Y'][1]
 
-stop_places_start = get_stop_places(X1, Y1)
-stop_places_dest = get_stop_places(X2, Y2)
+# # get stop places within a certain distance of the given coordinates
+# stop_places_start = get_stop_places(X1, Y1)
+# with open('stop_places_start.json', 'w') as f:
+#     json.dump(stop_places_start, f)
+# stop_places_dest = get_stop_places(X2, Y2)
+# with open('stop_places_dest.json', 'w') as f:
+#     json.dump(stop_places_dest, f)
 
-print(stop_places_start)
-print(stop_places_dest)
+with open('stop_places_start.json', 'r') as f:
+    stop_places_start = json.load(f)
+with open('stop_places_dest.json', 'r') as f:
+    stop_places_dest = json.load(f)
 
+# get the didok-numbers of the stop places
 didok_number_start = [entry['number'] for entry in stop_places_start['results']]
 didok_number_dest = [entry['number'] for entry in stop_places_dest['results']]
 
-print(didok_number_start)
-print(didok_number_dest)
+# # get the routes between the coordinates and the stop places
+# routes_start = [get_route(X1, Y1, entry, 'start') for entry in didok_number_start]
+# with open('routes_start.json', 'w') as f:
+#     json.dump(routes_start, f)
+# routes_dest = [get_route(X2, Y2, entry, 'dest') for entry in didok_number_dest]
+# with open('routes_dest.json', 'w') as f:
+#     json.dump(routes_dest, f)
 
-route_start = [get_route(X1, Y1, entry, "start") for entry in didok_number_start]
-route_dest = [get_route(X2, Y2, entry, "dest") for entry in didok_number_dest]
+with open('routes_start.json', 'r') as f:
+    routes_start = json.load(f)
+with open('routes_dest.json', 'r') as f:  
+    routes_dest = json.load(f)
 
-print(route_start)
-print(route_dest)
+# define lists for the coordinates of the routes
+coords_routes_start = []
+coords_routes_dest = []
 
-for feature in route_start["FeatureCollection"]:
-    routes = feature["features"]['geometry']['coordinates']
-    print(routes)
+# get the coordinates of the routes
+for feature in routes_start:
+    route = feature['features'][0]['geometry']['coordinates']
+    coords_routes_start.append(route)
+
+for feature in routes_dest:
+    route = feature['features'][0]['geometry']['coordinates']
+    coords_routes_dest.append(route)
+
+# define transformer to convert coordinates from WGS84 to LV95
+transformer = Transformer.from_crs('epsg:4326', 'epsg:2056')
+
+# define lists for the coordinates of the routes in LV95
+routes_start_lv95 = []
+routes_dest_lv95 = []
+
+# convert the coordinates of the routes to LV95
+for route in coords_routes_start:
+    route_lv95 = [(transformer.transform(latitude, longitude)) for longitude, latitude in route]
+    routes_start_lv95.append(route_lv95)
+
+for route in coords_routes_dest:
+    route_lv95 = [(transformer.transform(latitude, longitude)) for longitude, latitude in route]
+    routes_dest_lv95.append(route_lv95)
+
+print(routes_dest_lv95)
+
+######################## Function API request height profile ######################
+def get_height_profile(route):
+    geom = {
+        "type": "LineString",
+        "coordinates": route
+    }
+
+    # Convert the geom dictionary to a JSON string
+    geom_json = json.dumps(geom)
+
+    # Include the JSON string in the URL
+    url = f"https://api3.geo.admin.ch/rest/services/profile.json?geom={geom_json}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        stop_places = response.json()
+        return stop_places
+    else:
+        print(f"Error: Failed to retrieve data for route {route}")
+        return None
+
+profile = get_height_profile(routes_dest_lv95[0])
+print(profile)
---
+++ Routing_Test/functions.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+######################## Function API request stop places #########################
+# get stop places within a certain distance of the given coordinates
+def get_stop_places(X, Y, distance=500):
+    params = {
+        "where": f"within_distance(geopos_haltestelle, geom'Point({X} {Y})',{distance}m)",
+        "limit": 20
+    }
+
+    url = "https://data.sbb.ch/api/explore/v2.1/catalog/datasets/haltestelle-haltekante/records"
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        stop_places = response.json()
+        return stop_places
+    else:
+        print(f"Error: Failed to retrieve data for coordinates {X}, {Y}")
+        return None
+    
+######################## Function API request routing #########################
+# get the route between a coordinate and a stop place
+def get_route(X, Y, stop_place, type):
+    params = {
+        "client": "webshop",
+        "clientVersion": "latest",
+        "lang": "de",
+        "accessible": True,
+        "api_key": "59b20b25750f56bf888ef149873f24da"
+    }
+
+    # set parameters based on whether the coordinate is the starting point or the destination
+    if type == "start":
+        params["fromLatLng"] = f"{Y},{X}"
+        params["toStationID"] = stop_place
+    elif type == "dest":
+        params["fromStationID"] = stop_place
+        params["toLatLng"] = f"{Y},{X}"
+    else:
+        print(f"Error: Invalid type {type}. Expected 'start' or 'dest'.")
+        return None
+
+    url = "https://journey-maps.api.sbb.ch/v1/transfer"
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        route = response.json()
+        return route
+    else:
+        print(f"Error: Failed to retrieve data for stop place {stop_place}")
+        return None
