34ef0fdf308fbc3148c9a7d9841322b03f7e1324
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2024-05-13 15:14:58 +0200

    MapMain toilets info-box

--- client/src/app/components/map-main/map-main.component.ts
+++ client/src/app/components/map-main/map-main.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import { Map , Marker} from 'maplibre-gl';
+import { Component, OnDestroy, AfterViewInit } from '@angular/core';
+import { Map, Popup } from 'maplibre-gl';
 
 declare global {
   interface Window {
@@ -10,23 +10,29 @@ declare global {
 @Component({
   selector: 'map-main',
   templateUrl: './map-main.component.html',
-  styleUrl: './map-main.component.css'
+  styleUrls: ['./map-main.component.css']
 })
-export class MapMain implements OnDestroy, AfterViewInit{
+export class MapMain implements OnDestroy, AfterViewInit {
   apiKey = window.JM_API_KEY;
-
   private map!: Map;
 
   constructor() { }
 
   ngAfterViewInit(): void {
     this.map = new Map({
-      container: 'map', // container ID
-      style: `https://journey-maps-tiles.geocdn.sbb.ch/styles/base_bright_v2/style.json?api_key=${this.apiKey}`, // your MapTiler style URL
-      center: [7.56, 46.85], // starting position [lng, lat]
-      zoom: 10 // starting zoom
+      container: 'map', // Container-ID
+      style: `https://journey-maps-tiles.geocdn.sbb.ch/styles/base_bright_v2/style.json?api_key=${this.apiKey}`,
+      center: [7.56, 46.85], // Startposition [lng, lat]
+      zoom: 10 // Startzoom
     });
+
     this.map.on('load', () => {
+      const image = new Image(150, 150);
+      image.onload = () => {
+        this.map.addImage('cat', image);
+        this.addGeoJsonLayer();
+      };
+      image.src = 'assets/icons/Toiletten.svg';
     });
   }
 
@@ -36,4 +42,51 @@ export class MapMain implements OnDestroy, AfterViewInit{
     }
   }
 
+  private addGeoJsonLayer(): void {
+    fetch('assets/geojson/prm_toilets.geojson')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Netzwerkantwort war nicht ok');
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.map.addSource('prm_toilets', {
+          type: 'geojson',
+          data: data
+        });
+
+        this.map.addLayer({
+          id: 'prm_toilets-layer',
+          type: 'symbol',
+          source: 'prm_toilets',
+          layout: {
+            'icon-image': 'cat',
+            'icon-size': 0.1
+          }
+        });
+
+        this.map.on('click', 'prm_toilets-layer', (e) => {
+          if (e.features && e.features.length > 0) {
+              const feature = e.features[0];
+              if (feature.geometry.type === 'Point' && feature.geometry.coordinates.length === 2) {
+                  const coordinates: [number, number] = [
+                      feature.geometry.coordinates[0],
+                      feature.geometry.coordinates[1]
+                  ];
+      
+                  const wheelchairToilet = feature.properties['WHEELCHAIR_TOILET'];
+                  const wheelchairAccess = wheelchairToilet === 1 ? 'Ja' : (wheelchairToilet === 2 ? 'Nein' : 'Unbekannt');
+
+                  new Popup()
+                      .setLngLat(coordinates)
+                      .setHTML(`<strong>Rollstuhlgerechtes WC:</strong> ${wheelchairAccess}`)
+                      .addTo(this.map);
+              }
+          }
+      });
+      
+      })
+      .catch(error => console.error('Fehler beim Laden des GeoJSON: ', error));
+  }
 }
