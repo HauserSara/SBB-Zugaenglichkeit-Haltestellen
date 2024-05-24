d5a4301d30ba6801b15b6372573611edd3be0fe9
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2024-05-24 09:02:13 +0200

    Update client to use new endpoint (route_ojp)

--- client/src/app/components/map-nav/map-nav.component.ts
+++ client/src/app/components/map-nav/map-nav.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import { Map , Marker, LngLat, GeoJSONSource, AttributionControl} from 'maplibre-gl';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import maplibregl, { Map, Marker, LngLat, GeoJSONSource, AttributionControl } from 'maplibre-gl';
 import { HttpClient } from '@angular/common/http';
+import { Feature, Geometry } from 'geojson';
 
 declare global {
   interface Window { JM_API_KEY: string }
@@ -9,16 +10,15 @@ declare global {
 @Component({
   selector: 'map-nav',
   templateUrl: './map-nav.component.html',
-  styleUrl: './map-nav.component.css'
+  styleUrls: ['./map-nav.component.css']
 })
 export class MapNav implements OnInit, OnDestroy {
   apiKey = window.JM_API_KEY;
   private map!: Map;
   public markers: Marker[] = [];
   
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
   
-
   ngOnInit(): void {
     this.initMap();
   }
@@ -53,7 +53,6 @@ export class MapNav implements OnInit, OnDestroy {
       compact: true
     });
     this.map.addControl(attributionControl, 'top-left');
-
   }
 
   private handleMapClick(lngLat: LngLat): void {
@@ -80,120 +79,106 @@ export class MapNav implements OnInit, OnDestroy {
     this.markers.push(marker);
   }
 
-  //nur für testzwecke kann nacher gelöscht werden
-  sendagain(): void {
-    this.http.post<any>('http://127.0.0.1:8000/route_journeymaps/', {
-      "lat1": this.markers[0].getLngLat().lng,
-      "lon1": this.markers[0].getLngLat().lat,
-      "lat2": this.markers[1].getLngLat().lng,
-      "lon2": this.markers[1].getLngLat().lat,
-      "time": "12:00"
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'sbb-a11y-app'
-      }
-    }).subscribe({
-      next: (geojsonData) => this.displayGeoJSON(geojsonData),
-      error: (error) => console.error("POST call in error", error),
-      complete: () => console.log("The POST observable is now completed.")
-    });
-  }
-
-
   getRoute(): void {
     if (this.markers.length < 2) {
       return; // Ensure there are exactly two markers
     }
 
-  //   this.http.post<any>('http://127.0.0.1:8000/route_journeymaps/', {
-  //     "lat1": this.markers[0].getLngLat().lng,
-  //     "lon1": this.markers[0].getLngLat().lat,
-  //     "lat2": this.markers[1].getLngLat().lng,
-  //     "lon2": this.markers[1].getLngLat().lat,
-  //     "time": "12:00"
-  //   }, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'User-Agent': 'sbb-a11y-app'
-  //     }
-  //   }).subscribe({
-  //     next: (geojsonData) => this.displayGeoJSON(geojsonData),
-  //     error: (error) => console.error("POST call in error", error.error),
-  //     complete: () => console.log("The POST observable is now completed.")
-  //   });
-  // }
+    const url = 'http://127.0.0.1:8000/route_ojp/';
+    const body = {
+      "lat1": this.markers[0].getLngLat().lat,
+      "lon1": this.markers[0].getLngLat().lng,
+      "lat2": this.markers[1].getLngLat().lat,
+      "lon2": this.markers[1].getLngLat().lng,
+      "time": "12:00"
+    };
+    const headers = {
+      'Content-Type': 'application/json',
+      'User-Agent': 'sbb-a11y-app'
+    };
 
-  const url = 'http://127.0.0.1:8000/route_journeymaps/';
-  const body = {
-    "lat1": this.markers[0].getLngLat().lng,
-    "lon1": this.markers[0].getLngLat().lat,
-    "lat2": this.markers[1].getLngLat().lng,
-    "lon2": this.markers[1].getLngLat().lat,
-    "time": "12:00"
-  };
-  const headers = {
-    'Content-Type': 'application/json',
-    'User-Agent': 'sbb-a11y-app'
-  };
+    console.log(`Sending POST request to ${url} with body:`, body);
 
-  console.log(`Sending POST request to ${url} with body:`, body);
-
-  this.http.post<any>(url, body, { headers }).subscribe({
-    next: (geojsonData) => this.displayGeoJSON(geojsonData),
-    error: (error) => console.error("POST call in error", error.error),
-    complete: () => console.log("The POST observable is now completed.")
-  });
-}
-
-  displayGeoJSON(geoJSONData: any[]) {
-    const layerColor = '#FF0000'; // Definiere eine konstante Farbe für alle Schichten
-
-    geoJSONData.forEach((featureCollection, index) => {
-      const sourceId = `route-${index}`;
-      const layerId = `route-layer-${index}`;
-
-      if (!this.map.getSource(sourceId)) {
-        this.map.addSource(sourceId, {
-          type: 'geojson',
-          data: featureCollection
-        });
-
-        this.map.addLayer({
-          id: layerId,
-          type: 'line',
-          source: sourceId,
-          layout: {},
-          paint: {
-            'line-color': layerColor,
-            'line-width': 5
-          }
-        });
-      } else {
-        // Update the source data if it already exists
-        (this.map.getSource(sourceId) as GeoJSONSource).setData(featureCollection);
-      }
-
-      // Zoom zur Bounding Box des ersten Features, wenn index == 0
-      if (index === 0 && featureCollection.bbox) {
-        this.map.fitBounds(featureCollection.bbox as [number, number, number, number], {
-          padding: 20 // Optionaler Padding-Wert
-        });
-      }
-
+    this.http.post<any>(url, body, { headers }).subscribe({
+      next: (geojsonData) => this.displayGeoJSON(geojsonData),
+      error: (error) => console.error("POST call in error", error.error),
+      complete: () => console.log("The POST observable is now completed.")
     });
   }
 
-clearRoutes(): void {
+  displayGeoJSON(geoJSONData: any) {
+    const layerColor = '#FF0000'; // Define a constant color for all layers
+
+    Object.keys(geoJSONData).forEach((routeKey) => {
+      const route = geoJSONData[routeKey];
+
+      Object.keys(route).forEach((legKey, index) => {
+        const leg = route[legKey];
+
+        if (leg.type === 'ContinuousLeg' && leg.coordinates.length > 0) {
+          // Ensure coordinates are unique and the polyline is not closed
+          const uniqueCoordinates = leg.coordinates.map((coord: number[]) => ([coord[1], coord[0]] as [number, number]));
+          const filteredCoordinates = uniqueCoordinates.filter((coord: [number, number], index: number, self: [number, number][]) =>
+            index === self.findIndex((c: [number, number]) => c[0] === coord[0] && c[1] === coord[1])
+          );
+
+          const geojsonData: Feature<Geometry> = {
+            type: 'Feature',
+            geometry: {
+              type: 'LineString',
+              coordinates: filteredCoordinates
+            },
+            properties: {}  // Add empty properties object
+          };
+
+          const sourceId = `route-${routeKey}-${legKey}`;
+          const layerId = `route-layer-${routeKey}-${legKey}`;
+
+          if (!this.map.getSource(sourceId)) {
+            this.map.addSource(sourceId, {
+              type: 'geojson',
+              data: geojsonData
+            });
+
+            this.map.addLayer({
+              id: layerId,
+              type: 'line',
+              source: sourceId,
+              layout: {},
+              paint: {
+                'line-color': layerColor,
+                'line-width': 5
+              }
+            });
+          } else {
+            // Update the source data if it already exists
+            (this.map.getSource(sourceId) as GeoJSONSource).setData(geojsonData);
+          }
+
+          // Zoom to the bounding box of the first feature if it's the first leg
+          if (index === 0) {
+            const bounds = filteredCoordinates.reduce((bounds: maplibregl.LngLatBounds, coord: [number, number]) => bounds.extend(coord), new maplibregl.LngLatBounds(filteredCoordinates[0], filteredCoordinates[0]));
+            this.map.fitBounds(bounds, { padding: 20 });
+          }
+        }
+      });
+    });
+  }
+
+  clearRoutes(): void {
     if (this.map && this.map.getStyle()) {
-        this.map.getStyle().layers.forEach(layer => {
-            // Only attempt to remove the layer if it actually exists
-            if (this.map.getLayer(layer.id)) {
-                this.map.removeLayer(layer.id);
-            }
-        });
+      this.map.getStyle().layers.forEach(layer => {
+        if (layer.id.startsWith('route-layer-') && this.map.getLayer(layer.id)) {
+          this.map.removeLayer(layer.id);
+        }
+      });
+      Object.keys(this.map.getStyle().sources).forEach(sourceId => {
+        if (sourceId.startsWith('route-') && this.map.getSource(sourceId)) {
+          this.map.removeSource(sourceId);
+        }
+      });
     }
-}
+  }
 
   clearMarkers(): void {
     this.markers.forEach(marker => marker.remove());
