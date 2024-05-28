2a7fc4d4ef9eb65d4ca09a49538a2b70494b1d48
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2024-05-28 10:25:21 +0200

    change journey maps endpoint in test client

--- Test_Client/src/app/components/map-main/map-main.component.ts
+++ Test_Client/src/app/components/map-main/map-main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import maplibregl, { Map, Marker, LngLat, GeoJSONSource, AttributionControl } from 'maplibre-gl';
+import { Map, Marker, LngLat, GeoJSONSource } from 'maplibre-gl';
 import { HttpClient } from '@angular/common/http';
-import { Feature, Geometry } from 'geojson';
 
 declare global {
   interface Window { JM_API_KEY: string; }
@@ -12,20 +11,22 @@ declare global {
   templateUrl: './map-main.component.html',
   styleUrls: ['./map-main.component.css']
 })
-export class MapMain implements OnDestroy, AfterViewInit, OnInit {
+export class MapMain implements OnDestroy, AfterViewInit {
   apiKey = window.JM_API_KEY;
   private map!: Map;
   public markers: Marker[] = [];
-  
+
   constructor(private http: HttpClient) { }
-  
-  ngOnInit(): void {
-    
-  }
 
   ngAfterViewInit(): void {
-    this.initializeMap();
-    this.map.resize()
+    this.map = new Map({
+      container: 'map-main', // container ID
+      style: `https://journey-maps-tiles.geocdn.sbb.ch/styles/base_bright_v2/style.json?api_key=${this.apiKey}`, // your MapTiler style URL
+      center: [8.2275, 46.8182], // starting position [lng, lat]
+      zoom: 7.5 // starting zoom
+    });
+
+    this.initMap();
   }
 
   ngOnDestroy(): void {
@@ -35,19 +36,8 @@ export class MapMain implements OnDestroy, AfterViewInit, OnInit {
     this.clearMarkers();
   }
 
-  private initializeMap(): void {
-    if (this.map) {
-      this.map.remove();  // Clean up the existing map before creating a new one
-    }
-
-    this.map = new Map({
-      container: 'map-main', // container ID
-      style: `https://journey-maps-tiles.geocdn.sbb.ch/styles/base_bright_v2/style.json?api_key=${this.apiKey}`, // your MapTiler style URL
-      center: [8.2275, 46.8182], // starting position [lng, lat]
-      zoom: 7.5, // starting zoom
-      attributionControl: false
-    });
-
+  private initMap(): void {
+    
     this.map.on('load', () => {
       this.map.on('click', (e) => {
         this.handleMapClick(e.lngLat);
@@ -69,13 +59,7 @@ export class MapMain implements OnDestroy, AfterViewInit, OnInit {
           'raster-opacity': 0.4
         }
       });
-      
     });
-
-    const attributionControl = new AttributionControl({
-      compact: true
-    });
-    this.map.addControl(attributionControl, 'top-left');
   }
 
   private handleMapClick(lngLat: LngLat): void {
@@ -98,7 +82,7 @@ export class MapMain implements OnDestroy, AfterViewInit, OnInit {
     el.style.height = '60px';
     el.style.backgroundSize = 'cover';
 
-    const marker = new Marker({ element: el, anchor: 'bottom' }).setLngLat(lngLat).addTo(this.map);
+    const marker = new Marker({ element: el }).setLngLat(lngLat).addTo(this.map);
     this.markers.push(marker);
   }
 
@@ -107,12 +91,12 @@ export class MapMain implements OnDestroy, AfterViewInit, OnInit {
       return; // Ensure there are exactly two markers
     }
 
-    const url = 'http://127.0.0.1:8000/route_ojp/';
+    const url = 'http://127.0.0.1:8000/route_journeymaps/';
     const body = {
-      "lat1": this.markers[0].getLngLat().lat,
-      "lon1": this.markers[0].getLngLat().lng,
-      "lat2": this.markers[1].getLngLat().lat,
-      "lon2": this.markers[1].getLngLat().lng,
+      "lat1": this.markers[0].getLngLat().lng,
+      "lon1": this.markers[0].getLngLat().lat,
+      "lat2": this.markers[1].getLngLat().lng,
+      "lon2": this.markers[1].getLngLat().lat,
       "time": "12:00"
     };
     const headers = {
@@ -128,84 +112,48 @@ export class MapMain implements OnDestroy, AfterViewInit, OnInit {
       complete: () => console.log("The POST observable is now completed.")
     });
   }
-
-  displayGeoJSON(geoJSONData: any) {
-    const layerColor = '#FF0000'; // Define a constant color for all layers
-
-    Object.keys(geoJSONData).forEach((routeKey) => {
-      const route = geoJSONData[routeKey];
-
-      Object.keys(route).forEach((legKey, index) => {
-        const leg = route[legKey];
-
-        if (leg.type === 'ContinuousLeg' && leg.coordinates.length > 0) {
-          // Ensure coordinates are unique and the polyline is not closed
-          const uniqueCoordinates = leg.coordinates.map((coord: number[]) => ([coord[1], coord[0]] as [number, number]));
-          const filteredCoordinates = uniqueCoordinates.filter((coord: [number, number], index: number, self: [number, number][]) =>
-            index === self.findIndex((c: [number, number]) => c[0] === coord[0] && c[1] === coord[1])
-          );
-
-          const geojsonData: Feature<Geometry> = {
-            type: 'Feature',
-            geometry: {
-              type: 'LineString',
-              coordinates: filteredCoordinates
-            },
-            properties: {}  // Add empty properties object
-          };
-
-          const sourceId = `route-${routeKey}-${legKey}`;
-          const layerId = `route-layer-${routeKey}-${legKey}`;
-
-          if (!this.map.getSource(sourceId)) {
-            this.map.addSource(sourceId, {
-              type: 'geojson',
-              data: geojsonData
-            });
-
-            this.map.addLayer({
-              id: layerId,
-              type: 'line',
-              source: sourceId,
-              layout: {},
-              paint: {
-                'line-color': layerColor,
-                'line-width': 5
-              }
-            });
-          } else {
-            // Update the source data if it already exists
-            (this.map.getSource(sourceId) as GeoJSONSource).setData(geojsonData);
+  displayGeoJSON(responseData: any[]): void {
+    responseData.forEach((item) => {
+      const geoJSONData = item[1]; // Extrahieren des GeoJSON-Objekts
+      const layerColor = '#FF0000';
+      const sourceId = `route-${geoJSONData.features[0].properties.type}-${item[0]}`; // Eindeutige Source ID
+      const layerId = `route-layer-${geoJSONData.features[0].properties.type}-${item[0]}`;
+  
+      if (!this.map.getSource(sourceId)) {
+        this.map.addSource(sourceId, {
+          type: 'geojson',
+          data: geoJSONData
+        });
+  
+        this.map.addLayer({
+          id: layerId,
+          type: 'line',
+          source: sourceId,
+          layout: {},
+          paint: {
+            'line-color': layerColor,
+            'line-width': 5
           }
-
-          // Zoom to the bounding box of the first feature if it's the first leg
-          if (index === 0) {
-            const bounds = filteredCoordinates.reduce((bounds: maplibregl.LngLatBounds, coord: [number, number]) => bounds.extend(coord), new maplibregl.LngLatBounds(filteredCoordinates[0], filteredCoordinates[0]));
-            this.map.fitBounds(bounds, { padding: 20 });
-          }
-        }
-      });
+        });
+      } else {
+        // Update the source data if it already exists
+        (this.map.getSource(sourceId) as GeoJSONSource).setData(geoJSONData);
+      }
     });
   }
-
-  clearRoutes(): void {
-    if (this.map && this.map.getStyle()) {
-      this.map.getStyle().layers.forEach(layer => {
-        if (layer.id.startsWith('route-layer-') && this.map.getLayer(layer.id)) {
-          this.map.removeLayer(layer.id);
-        }
-      });
-      Object.keys(this.map.getStyle().sources).forEach(sourceId => {
-        if (sourceId.startsWith('route-') && this.map.getSource(sourceId)) {
-          this.map.removeSource(sourceId);
-        }
-      });
-    }
-  }
+  
 
   clearMarkers(): void {
     this.markers.forEach(marker => marker.remove());
     this.markers = [];
     this.clearRoutes();
+  }
+
+  clearRoutes(): void {
+    this.map.getStyle().layers?.forEach(layer => {
+      if (layer.id.includes('route-layer-')) {
+        this.map.removeLayer(layer.id)
+      }
+    });
   }
 }
